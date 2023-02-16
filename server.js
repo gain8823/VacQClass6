d2c90cb83b6f495a292a1a9978c03246db72115a
gain8823 <gain_8823cyber@msn.com>
2023-02-16 16:20:46 +0700

    Authentication

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 //Route files
 const hospitals = require('./routes/hospitals');
+const auth      = require('./routes/auth');
+
+const cookieParser = require('cookie-parser');
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
@@ -11,28 +14,13 @@ connectDB();
 
 const app = express();
 
+app.use(cookieParser());
+
 app.use(express.json());
 
 // Mount router
 app.use('/api/v1/hospitals',hospitals);
-
-// app.get('/api/v1/bookings', (req,res) => {
-
-//     res.status(200).json({ success:true , msg:''});
-
-// });
-
-// app.get('/api/v1/auth', (req,res) => {
-
-//     res.status(200).json(({ success:true , data:{id:1}}));
-
-// });
-
-// app.get('/api/v1/users', (req,res) => {
-
-//     res.status(200).json(({ success:true , data:{id:1}}));
-
-// });
+app.use('/api/v1/auth',auth);
 
 const PORT = process.env.PORT || 2000;
 
